2a7ff26da88c84e9057bd6373444c35e27c2af4d
pier-four <jonathan.gutierrez.jr@gmail.com>
2026-07-10 14:59:11 +0800

    Changes Applied to other files

--- PROG3 MCO1/src/Slot.java
+++ PROG3 MCO1/src/Slot.java
@@ -5,12 +5,11 @@ public class Slot {
     private ArrayList<Item> items;
     private int count;
     private int startingCount;
-    private final int slotID;
     private float price;
 
-    public Slot(int count, int slotID, float price, Item item) {
+    public Slot(int count, float price, Item item) {
         int i;
-        this.itemType = item;
+        itemType = item;
         items = new ArrayList<Item>();
 
         if (count < 10)
@@ -18,17 +17,15 @@ public class Slot {
         else
             this.count = count;
 
-        this.startingCount = this.count;
-
-        this.slotID = slotID;
+        startingCount = this.count;
 
         if (price <= 0.00f)
             this.price = 100.00f;
         else
             this.price = price;
 
-        for(i=0;i<count;i++){
-           this.items.add(itemType);
+        for(i=0;i<this.count;i++){
+           items.add(itemType);
         }
     }
 
@@ -38,31 +35,27 @@ public class Slot {
 
     public Item dispense() {
         Item item = null;
-        if (this.count>0) {
-            item = this.items.get(this.items.size()-1);
-            this.items.remove(this.items.size()-1);
-            this.count--;
+        if (count>0) {
+            item = items.get(this.items.size()-1);
+            items.remove(this.items.size()-1);
+            count--;
         }
         return item;
     }
 
-    public int getID(){
-        return this.slotID;
-    }
-
     public float getPrice(){
-        return this.price;
+        return price;
     }
 
     public Item getItemType(){
-        return this.itemType;
+        return itemType;
     }
 
     public boolean setItemType(Item item){
         boolean success = false;
         if(item.getCALORIES()!=this.itemType.getCALORIES() && !(item.getName().equals(this.itemType.getName()))){
-            if(this.items.isEmpty()) {
-                this.itemType = item;
+            if(items.size()==0){
+                itemType = item;
                 success = true;
             }
         }
@@ -70,16 +63,16 @@ public class Slot {
     }
 
     public void addItem(){
-        this.items.add(itemType);
-        this.count++;
+        items.add(itemType);
+        count++;
     }
 
     public int getStartingCount(){
-        return this.startingCount;
+        return startingCount;
     }
 
     public int getCount(){
-        return this.count;
+        return count;
     }
 
     public void setStartingCount(int startingCount){
