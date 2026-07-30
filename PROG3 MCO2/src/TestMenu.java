12de0bf20c6df30524759e11fdb3d9d3aa203ee1
magnadongalo <edriellexine@gmail.com>
2026-07-30 17:44:14 +0800

    start of MCO2

---
+++ PROG3 MCO2/src/TestMenu.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TestMenu extends JFrame implements ActionListener {
+    private JButton vendingFeatures, maintenance, returnToMenu;
+    private JPanel masterPanel;
+    private JPanel subPanel;
+    private JPanel buttonPanel;
+    private JLabel label = new JLabel("<html><center>TEST<br>VENDING MACHINE</center></html>");
+    private VendingMachine vendingMachine;
+
+    public TestMenu(VendingMachine vendingMachine) {
+        this.vendingMachine = vendingMachine;
+
+        this.setSize(600, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        masterPanel = new JPanel();
+        masterPanel.setSize(600, 600);
+        masterPanel.setLayout(new BoxLayout(masterPanel, BoxLayout.Y_AXIS));
+
+        subPanel = new JPanel();
+        label.setFont(new Font("Bahnschrift", Font.PLAIN, 50));
+        subPanel.add(label);
+
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 10));
+
+        vendingFeatures = new JButton("<html><center>VENDING<br>FEATURES</center></html>");
+        vendingFeatures.setPreferredSize(new Dimension(150, 50));
+        vendingFeatures.addActionListener(this);
+        maintenance = new JButton("<html><center>MAINTENANCE</center></html>");
+        maintenance.setPreferredSize(new Dimension(150, 50));
+        maintenance.addActionListener(this);
+        returnToMenu = new JButton("<html><center>RETURN<br>TO MENU</center></html>");
+        returnToMenu.setPreferredSize(new Dimension(150, 50));
+        returnToMenu.addActionListener(this);
+
+        buttonPanel.add(vendingFeatures);
+        buttonPanel.add(maintenance);
+        buttonPanel.add(returnToMenu);
+
+        masterPanel.add(subPanel);
+        masterPanel.add(buttonPanel);
+        this.add(masterPanel);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == vendingFeatures) {
+            //invoke vendingFeatures menu
+            this.dispose();
+        } else if (e.getSource() == maintenance) {
+            //invoke maintenance menu
+            this.dispose();
+        } else if (e.getSource() == returnToMenu) {
+            this.dispose();
+            new VMFrame();
+        }
+    }
+}
---
+++ PROG3 MCO2/src/VMFrame.java
@@ -0,0 +1,93 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VMFrame extends JFrame implements ActionListener{
+    private JPanel masterPanel = new JPanel();
+    private JPanel subpanel = new JPanel();
+    private JPanel buttonPanel;
+    private JLabel label = new JLabel("<html><center>VENDING<br>MACHINE<br>SIM</center></html>");
+    private JButton create, test, exit;
+    private boolean running = false;
+    private VendingMachine vendingMachine;
+
+    public VMFrame() {
+
+        this.setSize(new Dimension(600, 600));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        masterPanel.setSize(600, 600);
+        masterPanel.setLayout(new BoxLayout(masterPanel, BoxLayout.Y_AXIS));
+
+        label.setFont(new Font("Bahnschrift", Font.PLAIN, 80));
+        subpanel.add(label);
+
+        //buttonPanel = new JPanel(new GridLayout(1, 3, 15, 10));
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 10));
+
+        create = new JButton("<html><center>CREATE<br>MACHINE</center></html>");
+        create.setPreferredSize(new Dimension(150, 50));
+        create.addActionListener(this);
+        test = new JButton("<html><center>TEST<br>MACHINE</center></html>");
+        test.setPreferredSize(new Dimension(150, 50));
+        test.addActionListener(this);
+        exit = new JButton("<html><center>EXIT</center></html>");
+        exit.setPreferredSize(new Dimension(150, 50));
+        exit.addActionListener(this);
+
+        buttonPanel.add(create);
+        buttonPanel.add(test);
+        buttonPanel.add(exit);
+
+        masterPanel.add(subpanel);
+        masterPanel.add(buttonPanel);
+        this.add(masterPanel);
+        this.setVisible(true);
+
+        running = true;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == create) {
+            createVendingMachine();
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Vending Machine Created."
+            );
+            running = false;
+        } else if (e.getSource() == test) {
+            if (vendingMachine != null) {
+                //insert test menu here
+                this.dispose();
+                new TestMenu(vendingMachine);
+            }
+            else
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Create a vending machine first.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                );
+
+            running = false;
+        } else if (e.getSource() == exit) {
+            running = false;
+            this.dispose();
+        }
+    }
+
+    public void createVendingMachine() {
+        vendingMachine = new VendingMachine();
+    }
+
+    public VendingMachine getVendingMachine() {
+        return vendingMachine;
+    }
+
+    public static void main(String[] args) {
+        VMFrame test = new VMFrame();
+    }
+}
