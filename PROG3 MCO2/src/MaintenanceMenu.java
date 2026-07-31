c61a82f1bbba2045f143b9710295b94b581f31b2
magnadongalo <edriellexine@gmail.com>
2026-07-31 10:33:46 +0800

    Maintenance menu w/ barebones gui

--- PROG3 MCO2/src/MaintenanceMenu.java
+++ PROG3 MCO2/src/MaintenanceMenu.java
@@ -1,8 +1,74 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MaintenanceMenu extends JFrame {
+public class MaintenanceMenu extends JFrame implements ActionListener {
+    private JPanel masterPanel;
+    private JPanel subPanel;
+    private JPanel buttonPanel;
+    private JLabel label = new JLabel("<html><center>MAINTENANCE<br>FEATURES</center></html>");
+    private JButton restock, replenish, summary, returnToMenu;
+    private VendingMachine vendingMachine;
 
     public MaintenanceMenu(VendingMachine vendingMachine) {
+        this.setTitle("Maintenance Features");
+        this.vendingMachine = vendingMachine;
 
+        this.setSize(600, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        masterPanel = new JPanel();
+        masterPanel.setSize(600, 600);
+        masterPanel.setLayout(new BoxLayout(masterPanel, BoxLayout.Y_AXIS));
+
+        subPanel = new JPanel();
+        label.setFont(new Font("Bahnschrift", Font.PLAIN, 50));
+        subPanel.add(label);
+
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 10));
+
+        restock = new JButton("<html><center>RESTOCK ITEM</center></html>");
+        restock.setPreferredSize(new Dimension(150, 50));
+        restock.addActionListener(this);
+        replenish = new JButton("<html><center>REPLENISH CHANGE</center></html>");
+        replenish.setPreferredSize(new Dimension(150, 50));
+        replenish.addActionListener(this);
+
+        summary = new JButton("<html><center>TRANSACTION<br>SUMMARY</center></html>");
+        summary.setPreferredSize(new Dimension(150, 50));
+        summary.addActionListener(this);
+
+        returnToMenu = new JButton("<html><center>RETURN<br>TO MENU</center></html>");
+        returnToMenu.setPreferredSize(new Dimension(150, 50));
+        returnToMenu.addActionListener(this);
+
+        buttonPanel.add(restock);
+        buttonPanel.add(replenish);
+        buttonPanel.add(summary);
+        buttonPanel.add(returnToMenu);
+
+        masterPanel.add(subPanel);
+        masterPanel.add(buttonPanel);
+        this.add(masterPanel);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == restock) {
+            //invoke restock menu
+            this.dispose();
+        } else if (e.getSource() == replenish) {
+            //invoke replenish menu
+            this.dispose();
+        } else if (e.getSource() == summary) {
+            //invoke summary menu
+            this.dispose();
+        } else if (e.getSource() == returnToMenu) {
+            new TestMenu(vendingMachine);
+            this.dispose();
+        }
     }
 }
